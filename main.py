d4c2273b25638d00b9fcf4f6c8ad18fa2af7ee5b
Paul <paul@vulpes.xyz>
2019-04-12 17:17:52 +0100

    Sleepy sleeps

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import requests
+import time
 
-#f=open("te.txt", "r")
-#c = f.read()
-c = "noteworthy"
+f=open("te.txt", "r")
+c = f.read()
+#c = "Antebellum"
+r = []
 
 def checkPsnName(x):
     c = {'onlineId' : x, 'reserveIfAvailable' : False}
     return requests.post("https://accounts.api.playstation.com/api/v1/accounts/onlineIds", json=c)
 
 for w in c.split():
+    time.sleep(10)
     p = checkPsnName(w)
-    print(w, p, p.ok)
+    if p.status_code == 200:
+        print("Timed out, sleeping one min.. ")
+        time.sleep(61)
+        p = checkPsnName(w)
+        print(w, p, p.ok)
+        if p.status_code == 201:
+            print("Success!!")
+            r.append(w)
+    else:
+        print(w, p, p.ok)
+        if p.status_code == 201:
+            print("Success!!")
+            r.append(w)
+
+print(len(r), "Available Name(s) Found.")
+for n in r:
+    print("-", n)
